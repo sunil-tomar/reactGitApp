db824799f32f86ba26e090cefe5c834346b0d3a3
sunil-tomar <suniltomar.developer@gmail.com>
2024-03-27 23:22:51 +0530

    feat: adding vite gh-page config.
    feat: adding vite gh-page config.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure output directory is set to 'dist'
+    emptyOutDir: true, // Clear previous build files (optional)
+  },
+  base: 'reactGitApp', // Set base path (replace with your repo name)
+});
